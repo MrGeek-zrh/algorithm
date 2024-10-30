f37af2d1c821a36bb644e128c45ae0f81835431e
MrGeek <2262356245@qq.com>
2024-10-30 11:09:36 +0800

    最小堆题解

--- tmp.cpp
+++ tmp.cpp
@@ -1,65 +1,142 @@
-// 并查集
 #include <cstdio>
 #include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <cstring>
 
 using namespace std;
 
 const int N = 100010;
-int p[N] = { 0 };
+const long M = -1e9;
+int a[N] = { M };
 
-int query(int a)
+// 第k次插入的数的索引
+map<int, int> kth_map;
+
+int length = 0;
+int kth = 0;
+
+// 从idx开始向上调整
+int up(int idx)
 {
-    // 这里可以进行优化，除了通过a = p[a] ，递归的往上找集合的根节点以外，还可以同时对这棵树进行扁平化压缩，
-    // 虽然下面的写法， 在每次进行压缩的时候，并不能立刻压缩成一个扁平的树，但是经过多次压缩以后，就能变成一个很扁平的树
+    int current_idx = idx;
+    int parent_idx = current_idx / 2;
+    while (parent_idx >= 1) {
+        if (a[parent_idx] > a[current_idx]) {
+            int tmp;
+            tmp = a[parent_idx];
+            a[parent_idx] = a[current_idx];
+            a[current_idx] = tmp;
+            current_idx = parent_idx;
+            parent_idx = current_idx / 2;
+        } else {
+            // 新插入的不会破坏现有的小根堆
+            break;
+        }
+    }
+    return current_idx;
+}
+
+// 从idx开始向下调整
+int down(int idx)
+{
+    int current_idx = idx;
+    int child_idx;
+    while (current_idx <= length) {
+        if (2 * current_idx + 1 <= length) {
+            child_idx = a[2 * current_idx] <= a[2 * current_idx + 1] ? 2 * current_idx : 2 * current_idx + 1;
+        } else if (2 * current_idx <= length) {
+            child_idx = 2 * current_idx;
+        } else {
+            break;
+        }
+        if (a[current_idx] >= a[child_idx]) {
+            int tmp;
+            tmp = a[current_idx];
+            a[current_idx] = a[child_idx];
+            a[child_idx] = tmp;
+            current_idx = child_idx;
+        } else {
+            break;
+        }
+    }
+    return current_idx;
+}
+
+void insert(int x)
+{
+    kth++;
+    length++;
+    a[length] = x;
+    // 这是调整后的idx
+    int idx = up(length);
+    kth_map[kth] = idx;
+}
+
+int get_min()
+{
+    return a[1];
+}
+
+int son_min(int idx)
+{
+    return a[2 * idx] <= a[2 * idx + 1] ? a[2 * idx] : a[2 * idx + 1];
+}
+
+void modify(int k, int x)
+{
+    int idx = kth_map[k];
+    a[idx] = x;
+    // TODO:
+    // 这里维护kth_map我觉得有点问题
+    if (a[idx] <= a[idx / 2] && idx / 2 >= 1) {
+        kth_map[k] = up(idx);
+    } else if (a[idx] >= son_min(idx)) {
+        kth_map[k] = down(idx);
+    }
+}
+
+void del_k(int k)
+{
+    int x = a[length];
+    a[length] = M;
+    length--;
+    modify(k, x);
+}
+
+void del_min()
+{
+    del_k(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int n;
+    scanf("%d", &n);
+
+    memset(a, M, N * sizeof(int));
+
+    string op;
     int x;
-    while (p[a] != a) {
-        x = a;
-        a = p[a];
-        p[x] = p[a];
-    }
-    return a;
-}
-
-void merge(int a, int b)
-{
-    int p_a = query(a);
-    int p_b = query(b);
-    if (p_a != p_b) {
-        p[p_b] = p_a;
-    }
-}
-
-int main()
-{
-    int n, m;
-    scanf("%d %d", &n, &m);
-
-    char op;
-    int a, b;
-    for (int i = 0; i < m; i++) {
-        scanf("\n%c %d %d", &op, &a, &b);
-        // 如果当前节点是第一次插入，需要先修改他为根节点
-        if (p[a] == 0) {
-            p[a] = a;
-        }
-        if (p[b] == 0) {
-            p[b] = b;
-        }
-        switch (op) {
-            case 'M':
-                merge(a, b);
-                break;
-            case 'Q':
-                if (query(a) == query(b)) {
-                    printf("Yes\n");
-                } else {
-                    printf("No\n");
-                }
-                break;
-            default:
-                break;
+    int k;
+    for (int i = 0; i < n; i++) {
+        cin >> op;
+        if (op == "I") {
+            cin >> x;
+            insert(x);
+        } else if (op == "PM") {
+            int min = get_min();
+            cout << min << endl;
+        } else if (op == "DM") {
+            del_min();
+        } else if (op == "D") {
+            cin >> k;
+            del_k(k);
+        } else if (op == "C") {
+            cin >> k >> x;
+            modify(k, x);
         }
     }
-
     return 0;
 }
