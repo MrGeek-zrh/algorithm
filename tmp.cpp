e516969566fcec3864c08b4ac37e3c829effd87a
MrGeek <2262356245@qq.com>
2024-11-02 11:16:37 +0800

    字符串hash-暴力解

--- tmp.cpp
+++ tmp.cpp
@@ -1,86 +1,42 @@
-// 数组模拟散列表
+// 字符串hash-暴力解
 #include <cstdio>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-const int N = 100003;
+const int N = 100010;
+char a[N] = { '\0' };
 
-// 这里就放每个链表的头结点的索引
-int map[N] = { -1 };
-
-// 这里存放实际的数
-int a[N] = { 0 };
-int current_idx_a = 0;
-// 这里存放链接关系
-int ne[N] = { -1 };
-// 这里存放每个链表的长度
-int length[N] = { 0 };
-
-int h(int x)
+bool compare(int l1, int r1, int l2, int r2)
 {
-    // +N 在 %N是为了处理负数情况
-    return ((x % N) + N) % N;
-}
-
-void insert_list(int idx, int x)
-{
-    a[current_idx_a] = x;
-
-    length[idx]++;
-    if (length[idx] == 1) {
-        // 采用头插法实现链表，在链表为空的时候，需要将next指向-1。
-        ne[current_idx_a] = -1;
-    } else {
-        ne[current_idx_a] = map[idx];
+    int offset = 0;
+    int length1 = r1 - l1 + 1;
+    int length2 = r2 - l2 + 1;
+    if (length1 != length2) {
+        return false;
     }
-    map[idx] = current_idx_a;
-
-    current_idx_a++;
-}
-
-void insert(int x)
-{
-    int idx = h(x);
-    insert_list(idx, x);
-}
-
-bool query_list(int idx, int x)
-{
-    int head_idx = map[idx];
-    while (head_idx != -1) {
-        if (a[head_idx] == x) {
-            return true;
+    while (offset < length1 && offset < length2) {
+        if (a[l1 + offset] != a[l2 + offset]) {
+            return false;
         }
-        head_idx = ne[head_idx];
+        offset++;
     }
-    return false;
-}
-
-bool query(int x)
-{
-    int idx = h(x);
-    return query_list(idx, x);
+    return true;
 }
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int x;
-    char op;
-    for (int i = 0; i < n; i++) {
-        scanf("\n%c %d", &op, &x);
-        switch (op) {
-            case 'I':
-                insert(x);
-                break;
-            case 'Q':
-                printf("%s\n", query(x) ? "Yes" : "No");
-                break;
-            default:
-                break;
-        }
+    int n, m;
+    scanf("%d %d\n", &n, &m);
+    for (int i = 1; i <= n; i++) {
+        scanf("%c", &a[i]);
+    }
+
+    int l1, r1, l2, r2;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d %d", &l1, &r1, &l2, &r2);
+        printf("%s\n", compare(l1, r1, l2, r2) ? "Yes" : "No");
     }
     return 0;
 }
