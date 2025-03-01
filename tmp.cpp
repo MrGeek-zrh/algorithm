f2eaa191c3e410bfcd86b1398695958501a321ee
MrGeek-zrh <mrgeek-zrh@outlook.com>
2025-03-01 03:15:37 +0000

    拉链法散列表

--- tmp.cpp
+++ tmp.cpp
@@ -17,100 +17,55 @@
 
 using namespace std;
 
-const int N = 1e6 + 10;
+const int N = 100003;
 const int M = 1000100;
 
 int n, m;
 
-int a[N];
-// k：idx  第k个插入的数：在堆中的下标
-// 注意，不是用插入的数字本身作为key或者value，而是用下标，因为只有下标才是唯一的
-int k_idx[N];
-// idx：k
-int idx_k[N];
-// 整个堆的全局下标索引，也代表了当前堆的元素数量
-int idx = 0;
-int kth;
+// 一般用邻接表实现n条链表的时候，都是搭配链表的头插法，这样写起来代码最少，也最简单
+int a[N], e[N], ne[N], idx;
 
-// 交换两个堆中位置的三种数组的值，包括元素的值，以及两个映射关系
-void heap_swap(int idx_a, int idx_b)
+void insert(int x)
 {
-    swap(idx_k[idx_a], idx_k[idx_b]);
-    swap(k_idx[idx_k[idx_a]], k_idx[idx_k[idx_b]]);
-    swap(a[idx_a], a[idx_b]);
+    int x_idx = (x % N + N) % N;
+    e[idx] = x;
+    ne[idx] = a[x_idx];
+    a[x_idx] = idx;
+    idx++;
 }
 
-void up(int t_idx)
+bool query(int x)
 {
-    int t = t_idx;
-    int parent = t_idx / 2;
-    if (parent && a[parent] > a[t_idx]) {
-        t_idx = parent;
+    int x_idx = (x % N + N) % N;
+    int head = a[x_idx];
+    bool find = false;
+    while (head != -1) {
+        if (e[head] == x) {
+            find = true;
+            break;
+        }
+        head = ne[head];
     }
-    if (t != t_idx) {
-        heap_swap(t, t_idx);
-        up(t_idx);
-    }
-}
-
-void down(int t_idx)
-{
-    int t = t_idx;
-    int son_l = 2 * t_idx;
-    int son_r = 2 * t_idx + 1;
-    if (son_l <= idx && a[t_idx] > a[son_l]) {
-        t_idx = son_l;
-    }
-    if (son_r <= idx && a[t_idx] > a[son_r]) {
-        t_idx = son_r;
-    }
-    if (t != t_idx) {
-        heap_swap(t, t_idx);
-        down(t_idx);
-    }
+    return find;
 }
 
 int main()
 {
     cin >> n;
+
+    memset(a, -1, sizeof(a));
+
     string op;
-    int k, x;
+    int x;
     while (n--) {
         cin >> op;
+        cin >> x;
         if (op == "I") {
-            cin >> x;
-            idx++;
-            a[idx] = x;
-
-            kth++;
-
-            k_idx[kth] = idx;
-            idx_k[idx] = kth;
-
-            up(idx);
-        } else if (op == "PM") {
-            cout << a[1] << endl;
-        } else if (op == "DM") {
-            a[1] = a[idx];
-            heap_swap(1, idx);
-            idx--;
-
-            down(1);
-        } else if (op == "D") {
-            cin >> k;
-            int t_idx = k_idx[k];
-            a[t_idx] = a[idx];
-            heap_swap(t_idx, idx);
-            idx--;
-            up(t_idx);
-            down(t_idx);
-        } else if (op == "C") {
-            cin >> k >> x;
-            int t_idx = k_idx[k];
-            a[t_idx] = x;
-            up(t_idx);
-            down(t_idx);
+            insert(x);
+        } else if (op == "Q") {
+            cout << (query(x) ? "Yes" : "No") << endl;
         }
     }
+
     return 0;
 }
--- 数组模拟散列表.cpp
+++ 数组模拟散列表.cpp
@@ -1,87 +1,73 @@
-// 数组模拟散列表
-// 使用拉链法解决hash冲突
+#include <cctype>
+#include <cmath>
 #include <cstdio>
+#include <cstdlib>
+#include <functional>
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <pthread.h>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <cstring>
 
 using namespace std;
 
 const int N = 100003;
+const int M = 1000100;
 
-// 这里就放每个链表的头结点的索引
-int map[N] = { -1 };
+int n, m;
 
-// 这里存放实际的数
-int a[N] = { 0 };
-int current_idx_a = 0;
-// 这里存放链接关系
-int ne[N] = { -1 };
-// 这里存放每个链表的长度
-int length[N] = { 0 };
+// 数组模拟散列表，拉链法解决冲突
 
-int h(int x)
-{
-    // +N 在 %N是为了处理负数情况
-    return ((x % N) + N) % N;
-}
-
-void insert_list(int idx, int x)
-{
-    a[current_idx_a] = x;
-
-    length[idx]++;
-    if (length[idx] == 1) {
-        // 采用头插法实现链表，在链表为空的时候，需要将next指向-1。
-        ne[current_idx_a] = -1;
-    } else {
-        ne[current_idx_a] = map[idx];
-    }
-    map[idx] = current_idx_a;
-
-    current_idx_a++;
-}
+// 一般用邻接表实现n条链表的时候，都是搭配链表的头插法，这样写起来代码最少，也最简单
+int a[N], e[N], ne[N], idx;
 
 void insert(int x)
 {
-    int idx = h(x);
-    insert_list(idx, x);
-}
-
-bool query_list(int idx, int x)
-{
-    int head_idx = map[idx];
-    while (head_idx != -1) {
-        if (a[head_idx] == x) {
-            return true;
-        }
-        head_idx = ne[head_idx];
-    }
-    return false;
+    int x_idx = (x % N + N) % N;
+    e[idx] = x;
+    ne[idx] = a[x_idx];
+    a[x_idx] = idx;
+    idx++;
 }
 
 bool query(int x)
 {
-    int idx = h(x);
-    return query_list(idx, x);
+    int x_idx = (x % N + N) % N;
+    int head = a[x_idx];
+    bool find = false;
+    while (head != -1) {
+        if (e[head] == x) {
+            find = true;
+            break;
+        }
+        head = ne[head];
+    }
+    return find;
 }
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
+    cin >> n;
+
+    memset(a, -1, sizeof(a));
+
+    string op;
     int x;
-    char op;
-    for (int i = 0; i < n; i++) {
-        scanf("\n%c %d", &op, &x);
-        switch (op) {
-            case 'I':
-                insert(x);
-                break;
-            case 'Q':
-                printf("%s\n", query(x) ? "Yes" : "No");
-                break;
-            default:
-                break;
+    while (n--) {
+        cin >> op;
+        cin >> x;
+        if (op == "I") {
+            insert(x);
+        } else if (op == "Q") {
+            cout << (query(x) ? "Yes" : "No") << endl;
         }
     }
+
     return 0;
 }
